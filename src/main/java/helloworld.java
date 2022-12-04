accfa3718d9e28c1091d027d7711df5c4416b2ae
thebuilder1900 <45421126+thebuilder1900@users.noreply.github.com>
2022-12-04 17:04:11 +0100

    Update helloworld.java

--- src/main/java/helloworld.java
+++ src/main/java/helloworld.java
@@ -6,7 +6,6 @@ public class helloworld {
     public static void main(final String[] args) {
         final DateTimeFormatter dtf = DateTimeFormatter.ofPattern("h:mm:ss a 'on' MMMM d, yyyy'.'");
         final LocalDateTime now = LocalDateTime.now();
-        peterenis
 
         System.out.println("Hello, World! The current time is " + dtf.format(now));
     }
